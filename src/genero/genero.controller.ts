d67446b5a4e7b781c79ca4599c4e674f46a3fd60
PatricioChang <131512709+PatricioChang@users.noreply.github.com>
2024-07-18 12:08:15 -0400

    Eliminar generos

--- src/genero/genero.controller.ts
+++ src/genero/genero.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 import { GeneroService } from './genero.service';
 import { Genero } from 'src/entities/Genero.entity';
 import { CrearGeneroDto } from './DTO/CrearGenero.dto';
@@ -13,7 +13,12 @@ export class GeneroController {
     }
 
     @Post('crearGenero')
-    create(@Body() crearGeneroDto: CrearGeneroDto): Promise<Genero> {
+    async create(@Body() crearGeneroDto: CrearGeneroDto): Promise<Genero> {
         return this.generoService.crearGenero(crearGeneroDto)
+    }
+
+    @Delete(':id')
+    async deleteGenero(@Param('id') idGenero: number): Promise<void> {
+        await this.generoService.eliminarGenero(idGenero)
     }
 }
--- src/genero/genero.service.ts
+++ src/genero/genero.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Genero } from 'src/entities/Genero.entity';
 import { Repository } from 'typeorm';
@@ -24,5 +24,14 @@ export class GeneroService {
         await this.generoRepository.save(genero)
 
         return genero
+    }
+
+    async eliminarGenero(idGenero: number): Promise<void> {
+        const genero = await this.generoRepository.findOneById(idGenero)
+        if (!genero) {
+          throw new NotFoundException(`Libro con id ${idGenero} no encontrado.`)
+        }
+    
+        await this.generoRepository.remove(genero)
     }
 }
