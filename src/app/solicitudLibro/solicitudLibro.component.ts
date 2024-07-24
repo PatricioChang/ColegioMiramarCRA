47badd3fbe7278c8e72e35409e8a7827f4450f10
PatricioChang <131512709+PatricioChang@users.noreply.github.com>
2024-07-24 12:51:27 -0400

    Cursos Fix

--- src/app/solicitudLibro/solicitudLibro.component.ts
+++ src/app/solicitudLibro/solicitudLibro.component.ts
@@ -20,6 +20,7 @@ export class SolicitudLibroComponent implements OnInit {
   ) { 
     this.formularioSolicitudLibro= formBuilder.group({
       name: ['',Validators.required],
+      userType: ['', Validators.required],
       grade: ['', Validators.required],
       rut: [],
     })
@@ -32,6 +33,30 @@ export class SolicitudLibroComponent implements OnInit {
 
   public formularioSolicitudLibro: FormGroup
   public idLibro: number=0
+  public cursosEducacionGeneralBasica: string[] = [
+    '1°A', '1°B', '1°C', '2°A', '2°B', '2°C', 
+    '3°A', '3°B', '3°C', '4°A', '4°B', '4°C', 
+    '5°A', '5°B', '5°C', '6°A', '6°B', '6°C', 
+    '7°A', '7°B', '8°A', '8°B', '8°C'
+  ]
+  public cursosEnsenanzaMedia: string[] = [
+    '1°AHC', '1°BHC', '1°ATP', '1°BTP', 
+    '2°AHC', '2°BHC', '2°ATP', '2°BTP', 
+    '3°A HC', '3°A ADMINISTRACIÓN', '3°A PARVULOS', '3°B ENFERMERÍA', 
+    '4°A HC', '4°A ADMINISTRACIÓN', '4°A PARVULOS', '4°B ENFERMERÍA'
+  ]
+
+  public cambioUserType() {
+    const tipoUsuario = this.formularioSolicitudLibro.controls['userType'].value
+    if (tipoUsuario === 'funcionario') {
+      this.formularioSolicitudLibro.patchValue({ grade: 'Funcionario' })
+      this.formularioSolicitudLibro.controls['grade'].clearValidators()
+      this.formularioSolicitudLibro.controls['grade'].setValidators([Validators.required])
+    } else {
+      this.formularioSolicitudLibro.controls['grade'].setValidators([Validators.required])
+    }
+    this.formularioSolicitudLibro.controls['grade'].updateValueAndValidity()
+  }
 
   public comprobarReserva(): void{
     this.librosService.buscarLibroReservado(this.idLibro).subscribe(response=>{
@@ -61,6 +86,9 @@ export class SolicitudLibroComponent implements OnInit {
 
   public solicitarLibro(): void{
     if(this.formularioSolicitudLibro.valid){
+      /*if(this.formularioSolicitudLibro.controls['userType'].value=='funcionario'){
+        this.formularioSolicitudLibro.patchValue({ grade: 'Funcionario' })
+      }*/
       const solicitudLibroDto: SolicitudLibroDto= this.formularioSolicitudLibro.value
       solicitudLibroDto.fechaDeSolicitud= new Date()
       solicitudLibroDto.idLibro=this.idLibro
